d2f2b33fae659d5ac676e88e7aec426b8495db54
prajapatigagan <gaganprajapati121@gmail.com>
2026-01-05 23:52:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// let abcd = document.getElementById("abcd");
+// console.dir(abcd);
+
+// let abcd1 = document.getElementsByClassName("abcd");
+// console.dir(abcd1);
+
+// let abcd2 = document.querySelector("h1");
+// console.dir(abcd2);
+
+let abcd3 = document.querySelectorAll("h1");
+console.dir(abcd3);
+
