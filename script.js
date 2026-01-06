e3c2c87754198e7bb312fd4a5053acee78141c2c
prajapatigagan <gaganprajapati121@gmail.com>
2026-01-06 22:27:09 +0530

    second day

--- script.js
+++ script.js
@@ -7,6 +7,63 @@
 // let abcd2 = document.querySelector("h1");
 // console.dir(abcd2);
 
-let abcd3 = document.querySelectorAll("h1");
-console.dir(abcd3);
+// let abcd3 = document.querySelectorAll("h1");
+// console.dir(abcd3);
 
+
+//change for content...
+// let h1 = document.querySelector("h1");
+// h1.textContent = "HAY gagan kaise ho";
+// h1.innerText = "HAY gagan kaise hoooo";
+// h1.innerHTML = "HAY gagannnn kaise ho";
+
+//Attributes manipulation...
+// let h2=document.querySelector("a");
+// console.log(h2);
+// h2.href="https://www.google.com";
+// h2.setAttribute("href","https://www.google.com");
+
+//get attribute
+// let a = document.querySelector("h1");
+// console.log(a.getAttribute("href"));
+
+// set attribute
+// let a = document.querySelector("h1");
+// a.setAttribute("href","https://www.google.com");
+
+//images change..
+// let img=document.querySelector("img");
+// img.setAttribute("src","https://images.pexels.com/photos/13876644/pexels-photo-13876644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+
+//remove attributes..
+// let a=document.querySelector("a");
+// a.removeAttribute("href");
+
+//daynamic dom manipulation...
+
+//create element...
+// let s1=document.createElement("h1");
+// s1.textContent="gagan";
+// console.log(s1);
+// append element...
+// document.body.append(s1);
+//prepend element...
+// document.body.prepend(s1);
+//remove element...
+// let s2=document.querySelector("h1");
+// s2.remove();
+
+
+// h1=document.createElement("h1");
+// h1.textContent="gagan kaise ho";
+// document.querySelector("div").append(h1);
+// document.querySelector("div").prepend(h1);
+// document.querySelector("div").remove(h1);
+
+
+//syle...
+let h1=document.querySelector("h1");
+// console.dir(h1);
+// h1.classList.add("hulu");
+// h1.classList.remove("hulu");
+// h1.classList.toggle("hulu");
